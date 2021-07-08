20d7606af5042ff92a6fd5d8632227a6a3d9f2ba
Bamimore-Tomi <tomibami2020@gmail.com>
2021-07-08 13:58:35 -0400

    learning structs

--- beginner/main.go
+++ beginner/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main(){
-	fmt.Println("Hello World")
+import (
+	"fmt"
+)
+
+func main() {
+	type Person struct {
+		name string
+		age  int
+	}
+
+	type Team struct {
+		name    string
+		players [3]Person
+	}
+	var myTeam Team
+	fmt.Println((myTeam))
+
+	players := [...]Person{Person{name: "Forrest", age: 45}, Person{name: "Gordon", age: 34}, Person{name: "Terry", age: 56}}
+	celtic := Team{name: "Celtic FC", players: players}
+	fmt.Println(celtic)
+
 }
